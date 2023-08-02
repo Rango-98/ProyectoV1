d63fde7dc956ee6ac9debb7f32ede82dc52e690a
master23451 <2119300464@soy.utj.edu.mx>
2023-08-01 21:51:39 -0600

    Se agregaron las librerias Volley y asyc-http

--- app/src/main/java/com/example/proyectov1/LoginActivity.java
+++ app/src/main/java/com/example/proyectov1/LoginActivity.java
@@ -5,16 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btnSesion, btnSesion2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSesion = findViewById(R.id.btnSesion);
+        btnSesion2 = findViewById(R.id.btnSesion2);
+
+        btnSesion.setOnClickListener(v ->{
+            Utilidades utilidades = new Utilidades();
+            ingresarApp(utilidades.getUrl());
+        });
+
+        btnSesion2.setOnClickListener(v -> startActivity(new Intent(this, RegistrarActivity.class)));
     }
 
-    public void registrar(View view){
-        startActivity(new Intent(getApplicationContext(), RegistrarActivity.class));
+    private void ingresarApp(String url) {
     }
 }
