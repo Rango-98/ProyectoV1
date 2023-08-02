501d8c7e38b8d1683363b426303b3fc882b93946
master23451 <2119300464@soy.utj.edu.mx>
2023-08-01 19:49:01 -0600

    Se agregaron las librerias Volley y asyc-http

--- app/src/main/java/com/example/proyectov1/MainActivity.java
+++ app/src/main/java/com/example/proyectov1/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.proyectov1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask t = new  TimerTask(){
+            @Override
+            public void run(){
+                if(sesionGuardada()){
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }else{
+                    startActivity(new Intent(MainActivity.this, PaginaPrincipalActivity.class));
+                }
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(t, 2000);
+    }
+
+    private boolean sesionGuardada() {
+        SharedPreferences datosSesion = getSharedPreferences("datos.dat", MODE_PRIVATE);
+        return datosSesion.getBoolean("sesion", false);
     }
 }
