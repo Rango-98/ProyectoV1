8b85936669fe4b1c95f0079b135aedc03798ec5a
German_Stout <german.nemo@hotmail.com>
2023-08-02 00:08:08 -0600

    cambios realizados

---
+++ app/src/main/java/com/example/proyectov1/carritoActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectov1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class carritoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrito);
+    }
+}
