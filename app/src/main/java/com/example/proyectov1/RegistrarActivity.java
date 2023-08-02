3d58fbefee87b73e4b8a86772f6a2e6ce82921b0
master23451 <2119300464@soy.utj.edu.mx>
2023-08-01 22:43:40 -0600

    Se agregaron implementaciones Volley y AsyncHttp

--- app/src/main/java/com/example/proyectov1/RegistrarActivity.java
+++ app/src/main/java/com/example/proyectov1/RegistrarActivity.java
@@ -3,7 +3,6 @@ package com.example.proyectov1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
