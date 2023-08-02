16604fc6bd3803e5312ddd5f4dc4b720a82c4913
master23451 <2119300464@soy.utj.edu.mx>
2023-08-01 21:42:59 -0600

    Se agregaron las librerias Volley y asyc-http

--- app/src/main/java/com/example/proyectov1/MainActivity.java
+++ app/src/main/java/com/example/proyectov1/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.proyectov1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TimerTask t = new  TimerTask(){
+            @Override
+            public void run(){
+                if(validarRegistro()){
+                    startActivity(new Intent(MainActivity.this, PaginaPrincipalActivity.class));
+                }else{
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(t, 1000);
+    }
 
+    private boolean validarRegistro(){
+        SharedPreferences datosSesion = getSharedPreferences("dato.dat", MODE_PRIVATE);
+        return datosSesion.getBoolean("registro", false);
     }
 }
---
+++ app/src/main/java/com/example/proyectov1/Ustulidades.java
@@ -0,0 +1,10 @@
+package com.example.proyectov1;
+
+public class Ustulidades {
+
+    private String url = "";
+
+    public String getUrl() {
+        return url;
+    }
+}
