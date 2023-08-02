80e501b12375d8fcf3c4e67bfbe2abb437d48c90
German_Stout <german.nemo@hotmail.com>
2023-08-01 21:06:28 -0600

    cambios realizados

--- app/src/main/java/com/example/proyectov1/MainActivity.java
+++ app/src/main/java/com/example/proyectov1/MainActivity.java
@@ -2,12 +2,7 @@ package com.example.proyectov1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TimerTask t = new  TimerTask(){
-            @Override
-            public void run(){
-                if(sesionGuardada()){
-                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                }else{
-                    startActivity(new Intent(MainActivity.this, PaginaPrincipalActivity.class));
-                }
-                finish();
-            }
-        };
-        Timer timer = new Timer();
-        timer.schedule(t, 2000);
-    }
 
-    private boolean sesionGuardada() {
-        SharedPreferences datosSesion = getSharedPreferences("datos.dat", MODE_PRIVATE);
-        return datosSesion.getBoolean("sesion", false);
     }
 }
