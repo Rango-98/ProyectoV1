d247d68818b8515808dbbb65a53fd50883a4a8d9
master23451 <2119300464@soy.utj.edu.mx>
2023-08-01 22:28:10 -0600

    Se agrego el metodo de cerrar sesion

--- app/src/main/java/com/example/proyectov1/PaginaPrincipalActivity.java
+++ app/src/main/java/com/example/proyectov1/PaginaPrincipalActivity.java
@@ -2,6 +2,8 @@ package com.example.proyectov1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class PaginaPrincipalActivity extends AppCompatActivity {
@@ -10,5 +12,16 @@ public class PaginaPrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
+    }
+
+    private void cerrar_Sesion(){
+        SharedPreferences.Editor datosSesion = getSharedPreferences("dato.dat", MODE_PRIVATE).edit();
+        datosSesion.clear();
+        datosSesion.apply();
+
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
