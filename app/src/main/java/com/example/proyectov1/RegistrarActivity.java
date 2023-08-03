88e5804593124467b7da5a1c36407e12b9fd2b35
master23451 <2119300464@soy.utj.edu.mx>
2023-08-02 23:38:42 -0600

    Se agrego la funcionalidad al activity de registrate.

--- app/src/main/java/com/example/proyectov1/RegistrarActivity.java
+++ app/src/main/java/com/example/proyectov1/RegistrarActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarActivity extends AppCompatActivity {
 
@@ -30,16 +32,25 @@ public class RegistrarActivity extends AppCompatActivity {
 
         registrar.setOnClickListener(v ->{
             Utilidades utilidades = new Utilidades();
-            registrarUsuario(this ,utilidades.getUrl()); // -> registrarusuario.php
+            registrarUsuario(this ,utilidades.getUrl() + "registrar_usuario.php");
         });
     }
 
     private void registrarUsuario(Context context, String url){
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, response -> {
             Toast.makeText(context, "Usuario registrado con exito", Toast.LENGTH_LONG).show();
         }, error -> {
             Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
-        });
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> map = new HashMap<>();
+                map.put("nombre", usuario.getText().toString());
+                map.put("correo", correo.getText().toString());
+                map.put("password", contrasena.getText().toString());
+                return map;
+            }
+        };
 
         RequestQueue queue = Volley.newRequestQueue(context);
         queue.add(stringRequest);
