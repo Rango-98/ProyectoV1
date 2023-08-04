93465f669f1c1380ab591c6f2044a077079d41fb
German_Stout <german.nemo@hotmail.com>
2023-08-03 22:08:45 -0600

    cambios realizados

---
+++ app/src/main/java/com/example/proyectov1/FormaPagoActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectov1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormaPagoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forma_pago);
+    }
+}
